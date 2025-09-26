19523ed4435c2ee72f94f44c82f4fb6c5864547e
Mostafa_Amer <161795892+MostafaAmer978@users.noreply.github.com>
2025-09-26 10:53:38 +0300

    feat(login): finish Presentation Layer

--- lib/features/auth/login_screen/data/datasources/remote/login_remote_ds_impl.dart
+++ lib/features/auth/login_screen/data/datasources/remote/login_remote_ds_impl.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flowery_tracking_app/core/di/di.dart';
 import 'package:flowery_tracking_app/core/errors/api_results.dart';
 import 'package:flowery_tracking_app/core/errors/failures.dart';
+import 'package:flowery_tracking_app/core/helpers/shared_pref.dart';
 import 'package:flowery_tracking_app/core/network/api_services.dart';
+import 'package:flowery_tracking_app/core/utils/constants.dart';
 import 'package:flowery_tracking_app/features/auth/login_screen/data/datasources/remote/login_remote_ds_contract.dart';
 import 'package:flowery_tracking_app/features/auth/login_screen/domain/entities/login_response_entity.dart';
 import 'package:injectable/injectable.dart';
@@ -9,6 +12,7 @@ import 'package:injectable/injectable.dart';
 @Injectable(as: LoginRemoteDataSourceContract)
 class LoginRemoteDataSourceImpl implements LoginRemoteDataSourceContract {
   ApiServices apiServices;
+  final sharedPreferences = getIt.get<SharedPrefHelper>();
 
   LoginRemoteDataSourceImpl({required this.apiServices});
 
@@ -23,6 +27,10 @@ class LoginRemoteDataSourceImpl implements LoginRemoteDataSourceContract {
         "password": password,
       });
       var loginResponseEntity = loginResponseModel.toEntity();
+      sharedPreferences.saveData(
+        key: AppConstants.token,
+        val: loginResponseModel.token,
+      );
       return ApiSuccessResult<LoginResponseEntity>(data: loginResponseEntity);
     } on DioException catch (dioError) {
       return ApiErrorResult(
