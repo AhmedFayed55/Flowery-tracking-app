b145956e40e3e729c686fd851eda7027e41d74a9
Yahya Mohamed <ym615318@gmail.com>
2025-10-02 00:14:14 +0300

    slove import

--- lib/core/network/api_services.dart
+++ lib/core/network/api_services.dart
@@ -3,7 +3,7 @@ import 'package:flowery_tracking_app/core/network/api_constants.dart';
 import 'package:flowery_tracking_app/features/auth/change_password/data/model/request/change_password_request_dto.dart';
 import 'package:flowery_tracking_app/features/auth/change_password/data/model/response/change_password_response_dto.dart';
 import 'package:injectable/injectable.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
 part 'api_services.g.dart';
 
 @RestApi()
