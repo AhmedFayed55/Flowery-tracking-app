cb272a467334350210d2da8787f37cd5a5d84574
Ahmed Yehia <amedyhia23@gmail.com>
2025-10-15 18:02:37 +0300

    remove unused import for login screen in route generator

--- lib/config/routing/route_generator.dart
+++ lib/config/routing/route_generator.dart
@@ -1,7 +1,6 @@
 import 'package:flowery_tracking_app/config/routing/app_routes.dart';
 import 'package:flowery_tracking_app/features/auth/change_password/view/page/change_password_screen.dart';
 import 'package:flowery_tracking_app/features/auth/apply/presentation/pages/apply_screen.dart';
-import 'package:flowery_tracking_app/features/auth/login_screen/presentation/pages/login_screen.dart';
 import 'package:flowery_tracking_app/features/thanks_page/presentation/pages/thanks_page.dart';
 import 'package:flowery_tracking_app/features/orders_page/presentation/pages/orders_page.dart';
 import 'package:flowery_tracking_app/features/order_details/presentation/pages/order_details_screen.dart';
